29cb1fc74aa0cadd043da575857e150c7de883c3
albert <albert@mthreads.com>
2026-04-17 18:07:12 +0800

    update wukong dtype

--- exp/train_torch_wukong_on_criteo_kaggle_dataset.py
+++ exp/train_torch_wukong_on_criteo_kaggle_dataset.py
@@ -126,7 +126,7 @@ NUM_HIDDEN_HEAD = 2
 DIM_HIDDEN_HEAD = 256
 DROPOUT = 0.5
 BIAS = False
-DTYPE = torch.float16
+DTYPE = torch.float32
 
 ####################################################################################################
 #                                  TRAINING SPECIFIC CONFIGURATION                                 #
